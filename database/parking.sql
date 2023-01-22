4e88558b48a91b00db6fdb3abaee2f3ef6336e46
Akhil Joji <akhiljoji1451@gmail.com>
2023-01-22 10:39:36 +0530

    fix

--- database/parking.sql
+++ database/parking.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2023 at 03:20 PM
+-- Generation Time: Jan 22, 2023 at 06:08 AM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -63,7 +63,7 @@ INSERT INTO `lots` (`lot_id`, `lot_code`, `vehi_no`, `lot_status`, `type`) VALUE
 (6, 'AS0002', '', 'OUT', 'pre'),
 (7, 'AS0003', '', 'OUT', 'lot'),
 (8, 'AS0004', '', 'OUT', 'pre'),
-(9, 'AS0005', '', 'OUT', 'lot'),
+(9, 'AS0005', 'KL-05-W-1869', 'IN', 'lot'),
 (10, 'AS0006', '', 'OUT', 'pre'),
 (11, 'AS0007', '', 'OUT', 'pre');
 
@@ -81,19 +81,6 @@ CREATE TABLE `transaction` (
   `date_of_trans` date NOT NULL,
   `amount` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `transaction`
---
-
-INSERT INTO `transaction` (`tran_id`, `vehicle_no`, `receipt_no`, `time`, `date_of_trans`, `amount`) VALUES
-(1, 'KL-05-W-8697', '', '06:43:00', '2022-11-19', 200),
-(2, 'KL-05-AD-8657', '', '06:31:00', '2022-11-21', 200),
-(5, 'KL-05-AD-1234', '', '21:11:00', '2023-01-11', 50),
-(6, 'KL-05-AD-1234', '', '21:54:00', '2023-01-11', 50),
-(7, 'KL-05-W-1869', '', '19:16:00', '2023-01-12', 50),
-(8, 'KL-05-W-1869', '', '19:17:00', '2023-01-12', 50),
-(9, 'KL-05-AD-1234', '', '19:19:00', '2023-01-12', 50);
 
 -- --------------------------------------------------------
 
@@ -114,10 +101,6 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `age`, `email`, `vehi_no`) VALUES
-(1, 'akhil', 19, 'akhil@123', ''),
-(2, 'jerry', 19, 'jerry@gmail.com', ''),
-(6, 'akhil udayan', 19, 'akhiludayan@123', ''),
-(8, 'Joji', 41, 'jojimaliyil@gmail.com', ''),
 (9, 'akhil joji', 18, 'akhiljoji145@gmail.com', 'KL-05-W-1869'),
 (10, 'manu ', 28, 'manu@gmail.com', 'KL-05-AD-1234');
 
@@ -136,19 +119,16 @@ CREATE TABLE `vehicles` (
   `contact_no` varchar(50) DEFAULT NULL,
   `time_arr` time DEFAULT NULL,
   `date_arr` date DEFAULT NULL,
-  `status` varchar(50) NOT NULL
+  `status` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `vehicles`
 --
 
-INSERT INTO `vehicles` (`vehi_id`, `vehi_name`, `vehi_type`, `vehi_no`, `lot_no`, `contact_no`, `time_arr`, `date_arr`, `status`) VALUES
-(15, 'HONDA amaze', 'fourwheeler', 'KL-05-AD-1234', 'AS0003', '9497343468', '07:17:00', '2023-01-11', 'OUT'),
-(17, 'HONDA amaze', 'fourwheeler', 'KL-05-AD-1234', 'AS0006', '9497343468', '21:43:00', '2023-01-11', 'OUT'),
-(19, 'HONDA amaze', 'fourwheeler', 'KL-05-W-1869', 'AS0002', '9497343468', '19:15:00', '2023-01-12', 'OUT'),
-(20, 'HONDA amaze', 'fourwheeler', 'KL-05-W-1869', 'AS0002', '9497343468', '19:17:00', '2023-01-12', 'OUT'),
-(21, 'HONDA amaze', 'fourwheeler', 'KL-05-AD-1234', 'AS0005', '9497343468', '19:18:00', '2023-01-12', 'OUT');
+INSERT INTO `vehicles` (`vehi_id`, `vehi_name`, `vehi_type`, `vehi_no`, `lot_no`, `contact_no`, `time_arr`, `date_arr`, `status`, `type`) VALUES
+(22, 'Hero Splender', 'twowheeler', 'KL-05-W-1869', 'AS0005', '9497343468', '22:10:00', '2023-01-18', 'IN', 'lot');
 
 --
 -- Indexes for dumped tables
@@ -210,7 +190,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `vehi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
