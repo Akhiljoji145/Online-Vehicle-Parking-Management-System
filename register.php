281b562d8090f2826cb3bf2a6d11412e652e3239
Akhil Joji <akhiljoji1451@gmail.com>
2023-01-25 20:01:04 +0530

    update

--- register.php
+++ register.php
@@ -4,10 +4,17 @@ $username=$_POST['name'];
 $age=$_POST['age'];
 $email=$_POST['email'];
 $vehi_no=$_POST['vehi_no'];
-$sql="insert into users values('','$username','$age','$email','$vehi_no')";
-$query=mysqli_query($conn,$sql);
-if($query>0)
+$sql1="select * from vehicles where vehi_no='$vehi_no";
+$query1=mysqli_query($conn,$sql1);
+$result=mysqli_fetch_assoc($query1);
+if($vehi_no==$result['vehi_no'])
 {
+	echo'<script>alert("already registered")</script>';
+}
+else if($query>0)
+{
+	$sql="insert into users values('','$username','$age','$email','$vehi_no')";
+    $query=mysqli_query($conn,$sql);
 	echo'<script>alert("registered successfully")</script>';
 }
 else
