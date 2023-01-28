87c33e47feba08153c68ec6b01804738a3f737e4
Akhil <akhiljoji1451@gmail.com>
2023-01-28 09:37:20 +0530

    update

---
+++ home.js
@@ -0,0 +1,10 @@
+function validate() {
+  var vehi_no = document.form.vehi_no.value;
+
+  if (vehi_no == "") {
+    alert("Vehicle number cannot be empty");
+    return false;
+  }
+
+  return true;
+}
