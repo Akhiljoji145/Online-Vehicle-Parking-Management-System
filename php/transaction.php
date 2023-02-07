24eb0d599933cfc66c3e19e07b7becaba4cbd042
Akhiljoji145 <akhiljoji1451@gmail.com>
2023-02-07 11:32:17 +0530

    update

--- php/transaction.php
+++ php/transaction.php
@@ -10,12 +10,14 @@ $result=mysqli_num_rows($query);
 $row=mysqli_fetch_assoc($query);
 if($row['type']='pre')
 {
-    $sql1="UPDATE vehicles SET vehi_no='',status='OUT' WHERE vehi_no='$vehi_no'";
+    $sql1="UPDATE vehicles SET status='OUT' WHERE vehi_no='$vehi_no'";
     $sql2="UPDATE pre_lot SET vehi_no='',lot_status='OUT' WHERE vehi_no='$vehi_no'";
+    $SQL="insert into transaction values('','$vehi_no','','$time','$date','$amount')";
     $query1=mysqli_query($conn,$sql1);
     $query2=mysqli_query($conn,$sql2);
+    $query6=mysqli_query($conn,$SQL);
     
-    if($query1==TRUE && $query2==TRUE)
+    if($query1==TRUE && $query2==TRUE && $query6==TRUE)
     {
         echo"updated";
     }
@@ -28,10 +30,11 @@ else
 {
     $sql3="UPDATE vehicles SET vehi_no='',status='OUT' WHERE vehi_no='$vehi_no'";
     $sql4="UPDATE lots SET vehi_no='',lot_status='OUT' WHERE vehi_no='$vehi_no'";
+    $SQL1="insert into transaction values('','$vehi_no','','$time','$date','$amount')";
     $query3=mysqli_query($conn,$sql3);
     $query4=mysqli_query($conn,$sql4);
-    
-    if($query3==TRUE && $query4==TRUE)
+    $query5=mysqli_query($conn,$SQL1);
+    if($query3==TRUE && $query4==TRUE && $query5==TRUE)
     {
         echo"updated";
     }
